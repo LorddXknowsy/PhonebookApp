393021365d952e2d79584ba785c96764a7209a24
Lordd Knowsy <beriaxtasy@gmail.com>
2021-08-05 10:18:46 +0400

    First Commit

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -10,47 +10,67 @@ namespace ContactApp.Controllers
 {
     public class ContactController : Controller
     {
-        bool hasID = false;
+        private bool IsAuth()
+        {
+            if (Session["AuthUser"] != null) return true;
+            else return false;
+        }
+
         [HttpGet]
         public ActionResult GetContacts()
         {
-            
+
             return View();
         }
 
         [HttpPost]
-        public JsonResult GetContactsList()
+        public ActionResult GetContactsList()
         {
-            var authUser = (User)Session["AuthUser"];
-            var contactRepository = new ContactRepository();
-            var contacts = contactRepository.GetContacts(authUser.UserName);
-            return Json(contacts, JsonRequestBehavior.AllowGet);
+            if (!IsAuth()) return new HttpUnauthorizedResult();
+            else
+            {
+                var authUser = (User)Session["AuthUser"];
+                var contactRepository = new ContactRepository();
+                var contacts = contactRepository.GetContacts(authUser.UserName);
+                return Json(contacts, JsonRequestBehavior.AllowGet);
+            }
         }
 
         [HttpPost]
         public ActionResult SaveContact(Contact contact)
         {
-            hasID = false;
-            var authUser = (User)Session["AuthUser"];
-            contact.userName = authUser.UserName;
-            var contactRepository = new ContactRepository();
-            contactRepository.SaveContact(contact);
-            return RedirectToAction("GetContactsList");
+            if (!IsAuth()) return new HttpUnauthorizedResult();
+            else
+            {
+                var authUser = (User)Session["AuthUser"];
+                contact.userName = authUser.UserName;
+                var contactRepository = new ContactRepository();
+                contactRepository.SaveContact(contact);
+
+                return RedirectToAction("GetContactsList");
+            }
         }
         [HttpPost]
-        public JsonResult DeleteContact(Contact contact)
+        public ActionResult DeleteContact(Contact contact)
         {
-            var contactRepository = new ContactRepository();
-            contactRepository.DeleteContact(contact);
-            return Json(contact);
+            if (!IsAuth()) return new HttpUnauthorizedResult();
+            else
+            {
+                var contactRepository = new ContactRepository();
+                contactRepository.DeleteContact(contact);
+                return Json(contact);
+            }
         }
         [HttpPost]
-        public JsonResult UpdateContact(Contact contact)
+        public ActionResult UpdateContact(Contact contact)
         {
-            hasID = true;
-            var contactRepository = new ContactRepository();
-            contactRepository.UpdateContact(contact);
-            return Json(contact);
+            if (!IsAuth()) return new HttpUnauthorizedResult();
+            else
+            {
+                var contactRepository = new ContactRepository();
+                contactRepository.UpdateContact(contact);
+                return Json(contact);
+            }
         }
     }
 }
